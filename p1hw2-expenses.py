e6e1c3dff475eeb3ec088267173510465b3274f2
Mckhenziee02 <mckhenziej5@gmail.com>
2022-11-03 11:38:45 -0400

    Update p1hw2-expenses.py

--- p1hw2-expenses.py
+++ p1hw2-expenses.py
@@ -6,31 +6,44 @@
 #input: three expenses, the budget
 #processing:total the expenses, compare with budget
 #output:did you go over or under
+def main():
+    #our variables - type float, because of the decimal place
+    gas = 0.0
+    food = 0.0
+    hotel = 0.0
+    totalExpenses = 0.0 # gas + food + hotel
+    budget = 0.0
+    destination = ""
+    print("This program calculates on displays travel expenses")
+    print("Enter your budget ")
+    budget = float(input())
+    destination = input("Where are you heading? ")
 
-#our variables - type float, because of the decimal place
-gas = 0.0
-food = 0.0
-hotel = 0.0
-totalExpenses = 0.0 # gas + food + hotel
-budget = 0.0
-destination = ""
+    print("OK, lets total your expenses.")
 
-print("This program calculates on displays travel expenses")
+    gas = float(input("Gas: $") )
+    hotel = float(input("Hotel: $") )
+    food = float(input("Food: $") )
 
-print("Enter your budget ")
-budget = float(input())
+    #add everything up
+    totalExpenses = gas + hotel + food
+    #output - did they go over the budget?
+    print("Your total expenses are:  $", totalExpenses)
+    print("Your budget was:  $", budget)
 
-destination = input("Where are you heading? ")
+    if  totalExpenses > budget :
+           print ("You went over budget!")
+           print("by: $", budget - totalExpenses)
+    else:
+          print("You stayed within your budget.")
+          print("money left: $", budget - totalExpenses)
 
-print("OK, lets total your expenses.")
-print("Gas: $")
-gas = float(input())
+    goagin =input("Run again? y/n")
+    if goagin == "y":
+        main( )
+    else:
+        print("goodbye.")
 
-print("Hotel: $")
-hotel = float(input())
+#start the program
+main( )
 
-print("Food $")
-food = float(input())
-
-#add everything up
-totalExpenses = gas + hotel + food
